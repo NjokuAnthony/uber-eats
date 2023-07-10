8dff6c54a878e0c0db7b09aeea6df6dc3852099c
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-07-10 11:35:52 +0100

    created api

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {data} from '../data/data.js';
+import {data} from '../data/Data.js';
 
 const Food = () => {
     console.log(data);
@@ -7,31 +7,37 @@ const Food = () => {
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
         <h1 className='text-orange-600 font-bold text-4xl text-center'>Top Rated Menu Items</h1>
-        <div>
+            <div className='flex flex-col lg:flex-row justify-between '>
+                
+                <div>
+                    <p className='font-bold text-gray-700'>Filter Type</p>
+                        <div className='flex justify-between flex-wrap'>
+                            <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white'>All</button>
+                            <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white'>Burgers</button>
+                            <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white'>Pizza</button>
+                            <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white'>Salads</button>
+                            <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white'>Chickens</button>
+                        </div>
+                </div>
+
 
             <div>
-                <p>Filter Type</p>
-                <div>
-                    <button>All</button>
-                    <button>Burgers</button>
-                    <button>Pizza</button>
-                    <button>Salads</button>
-                    <button>Chickens</button>
-                </div>
-            </div>
-            <div>
-                <p>Filter Prices</p>
-                <div>
-                    <button>$</button>
-                    <button>$$</button>
-                    <button>$$$</button>
-                    <button>$$$$</button>
-                    <button>$$$$$</button>
-                </div>
+                <p className='font-bold text-gray-700'>Filter Prices</p>
+                    <div className='flex justify-between max-w-[390px] w-full'>
+                        <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white>$</button>
+                        <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white>$$</button>
+                        <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white>$$$</button>
+                        <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white>$$$$</button>
+                        <button className='m-1 border-orange-600 text-organge-600 hover:bg-orange-600 hover:text-white>$$$$$</button>
+                    </div>
             </div>
         </div>
+
+        <div>
+                
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default Food
